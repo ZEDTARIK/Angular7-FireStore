a875d593b8abdd078e0f953ed2046021443592fd
Zouhair <ettarak.zouhair@gmail.com>
2020-09-26 16:07:45 +0200

    list Employees From Server

--- src/app/components/employees/employee-list/employee-list.component.ts
+++ src/app/components/employees/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/core/employee';
+import { EmployeeService } from 'src/app/core/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor() { }
+  employees : Employee[];
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.employeeService.getData().subscribe((data: any) =>{
+      this.employees = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Employee;
+      });
+    });
   }
 
 }
